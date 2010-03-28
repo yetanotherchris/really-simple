ab9dada004d3d44d128ea5b5f9ca58131d172bf8
Chris Small <mrshrinkray@gmail.com>
2010-03-28 12:44:42 +0100

    SettingsController refactor

--- iPhone/ReallySimple.iPhone.UI/Controllers/ControllerBase.cs
+++ iPhone/ReallySimple.iPhone.UI/Controllers/ControllerBase.cs
@@ -18,16 +18,13 @@ namespace ReallySimple.iPhone.UI.Controllers
 	/// </summary>
 	public class ControllerBase : UIViewController
 	{
+		private InformationController _informationController;
 		private SettingsController _settingsController;
 
 		private UIBarButtonItem _settingsButton;
 		private UIBarButtonItem _informationButton;
 		private UIBarButtonItem _flexi1;
 		private UIBarButtonItem _flexi2;
-		
-		private UIButton _emailButton;
-		private UIButton _debugButton;
-		private UIView _containerView;
 		
 		/// <summary>
 		/// Used for saving in the Settings, so this view is loaded on startup.
@@ -77,15 +74,19 @@ namespace ReallySimple.iPhone.UI.Controllers
 			// Settings icon
 			_settingsButton = new UIBarButtonItem();
 			_settingsButton.Image = UIImage.FromFile("Assets/Images/Toolbar/settings.png");
-			_settingsButton.Clicked += new EventHandler(SettingsButtonClicked);
+			_settingsButton.Clicked += delegate
+			{
+				_settingsController = new SettingsController(null);
+				NavigationController.PushViewController(_settingsController, true);
+			};
 			
 			// Information icon
 			_informationButton = new UIBarButtonItem();
 			_informationButton.Image = UIImage.FromFile("Assets/Images/Toolbar/information.png");
 			_informationButton.Clicked += delegate
 			{
-				InformationController controller = new InformationController();
-				NavigationController.PushViewController(controller,true);
+				_informationController = new InformationController();
+				NavigationController.PushViewController(_informationController,true);
 			};
 			
 			// Two spacers to go between each icon
@@ -94,114 +95,6 @@ namespace ReallySimple.iPhone.UI.Controllers
 			_flexi2.Width = 20;
 			
 			return new UIBarButtonItem[] { _flexi1,_settingsButton,_flexi2,_informationButton };
-		}
-
-		private void SettingsButtonClicked(object sender, EventArgs e)
-		{
-			BindingContext bindingContext = new BindingContext(this, Settings.Current.UserSettings, "Settings");
-			AddSettingsButtons(bindingContext);
-			
-			_settingsController = new SettingsController(bindingContext.Root);
-			_settingsController.ViewDissapearing += delegate
-			{
-				bool showImages = Settings.Current.UserSettings.ImagesEnabled;
-				bindingContext.Fetch();
-
-				// Trigger all feeds reloading again - only if it was false beforehand (as we need the images HTML).
-				// If the setting was previously TRUE and we're switching to FALSE, we already have the content and don't need to update again.
-				if (showImages == false && Settings.Current.UserSettings.ImagesEnabled)
-				{
-					Settings.Current.ClearCache(true);
-				}
-			};
-
-			NavigationController.PushViewController(_settingsController, true);
-		}
-			
-		private void AddSettingsButtons(BindingContext bindingContext)
-		{
-			_containerView = new UIView();
-			_containerView.Frame = new RectangleF(10,280,300,100);
-			
-			// Add send email button
-			_emailButton = UIButton.FromType(UIButtonType.RoundedRect);
-			_emailButton.Frame = new RectangleF(10,0,300,40);
-			_emailButton.SetTitle("  Send error report by email",UIControlState.Normal);
-			_emailButton.HorizontalAlignment = UIControlContentHorizontalAlignment.Center;
-			_emailButton.TouchDown += delegate
-			{
-				
-				if (File.Exists(Settings.Current.LogFile))
-				{				
-					if (MFMailComposeViewController.CanSendMail)
-					{
-						NSData data = NSData.FromFile(Settings.Current.LogFile);
-						
-						MFMailComposeViewController mailController = new MFMailComposeViewController();
-						mailController.SetSubject("Really simple iPhone error report");
-						mailController.SetToRecipients(new string[]{"mrshrinkray@gmail.com"});
-						mailController.SetMessageBody("Really simple error report", false);
-						mailController.AddAttachmentData(data,"text/plain","error.log");
-						mailController.Finished += HandleMailFinished;
-						this.PresentModalViewController(mailController, true);
-					} 
-					else 
-					{
-						ModalDialog.Alert("No email account found","Please configure an email account before sending an error report");
-					}
-				}
-				else
-				{
-					ModalDialog.Alert("No log file was found","The error log was empty. Please try sending this again after the error occurs.");
-				}
-	
-			};
-			_containerView.AddSubview(_emailButton);
-			
-			// Debug details (disabled for release builds)
-			_debugButton = UIButton.FromType(UIButtonType.RoundedRect);
-			_debugButton.Frame = new RectangleF(10,45,300,40);
-			_debugButton.SetTitle(" Debug information",UIControlState.Normal);
-			_debugButton.HorizontalAlignment = UIControlContentHorizontalAlignment.Center;
-			_debugButton.TouchDown += delegate
-			{
-				ModalDialog.Alert("Debug",DebugInfo());	
-			};
-			
-			_containerView.AddSubview(_debugButton);
-			
-			Section section = new Section(_containerView);
-			bindingContext.Root.Add(section);
-		}
-		
-		private string DebugInfo()
-		{
-			var allItems = Repository.Default.ListItems();
-			var readItems = allItems.Where(i => !i.IsRead);
-			
-			// Image count
-			int imageCount = 0;
-            var dir = new DirectoryInfo(Settings.Current.ImageFolder);
-            var subdirs = dir.GetDirectories();
-            if (subdirs.Length > 0)
-            {
-                imageCount = subdirs[0].GetFiles().Length;
-            }
-			
-			StringBuilder builder = new StringBuilder();
-			builder.AppendLine(string.Format("Version {0}",Settings.Current.Version));
-			builder.AppendLine(string.Format("Last updated {0}",Settings.Current.LastUpdate.ToString("HH:mm")));
-			builder.AppendLine(string.Format("Items in the cache {0}",allItems.Count));
-			builder.AppendLine(string.Format("Unread items in the cache {0}",readItems.ToList().Count));
-			builder.AppendLine(string.Format("Images downloaded {0}", imageCount));
-			builder.AppendLine(string.Format("Image downloader working: {0}", ImageDownloader.Current.IsWorking));
-			
-			return builder.ToString();
-		}
-		
-		void HandleMailFinished(object sender, MFComposeResultEventArgs e)
-		{
-			e.Controller.DismissModalViewControllerAnimated(true);
 		}
 		
 		/// <summary>
--- iPhone/ReallySimple.iPhone.UI/Controllers/SettingsController.cs
+++ iPhone/ReallySimple.iPhone.UI/Controllers/SettingsController.cs
@@ -13,15 +13,16 @@ using System.Text;
 
 namespace ReallySimple.iPhone.UI.Controllers
 {
-	/// <summary>
-	/// The settings screen. 
-	/// ** The Monotouch.Dialog callback is found in ControllerBase **
-	/// </summary>
 	public class SettingsController : DialogViewController
 	{
 		private UIBarButtonItem _doneButton;
 		private UIBarButtonItem _clearButton;
 		private PickCategoriesController _pickCategories;
+		private BindingContext _bindingContext;
+
+		private UIButton _emailButton;
+		private UIButton _debugButton;
+		private UIView _containerView;
 		
 		protected string ControllerId
 		{
@@ -36,13 +37,15 @@ namespace ReallySimple.iPhone.UI.Controllers
 		/// </summary>
 		public SettingsController(RootElement root) : base(root)
 		{
-			
+			_bindingContext = new BindingContext(this, Settings.Current.UserSettings, "Settings");
+			ReloadData();
 		}
 
 		/// <summary>
 		/// Required by MonoTouch.Dialog
 		/// </summary>
-		public SettingsController(RootElement root, bool pushing) : base(root, pushing)
+		public SettingsController(RootElement root, bool pushing)
+			: base(root, pushing)
 		{
 			
 		}
@@ -81,8 +84,107 @@ namespace ReallySimple.iPhone.UI.Controllers
 		{
 			base.ViewWillDisappear(animated);
 
+			bool showImages = Settings.Current.UserSettings.ImagesEnabled;
+			_bindingContext.Fetch();
+
+			// Trigger all feeds reloading again - only if it was false beforehand (as we need the images HTML).
+			// If the setting was previously TRUE and we're switching to FALSE, we already have the content and don't need to update again.
+			if (showImages == false && Settings.Current.UserSettings.ImagesEnabled)
+			{
+				Settings.Current.ClearCache(true);
+			}
+
 			// Persist the controller name for next load
 			Settings.Current.LastControllerId = ControllerId;
+		}
+
+		private void AddSettingsButtons(BindingContext bindingContext)
+		{
+			_containerView = new UIView();
+			_containerView.Frame = new RectangleF(10, 280, 300, 100);
+
+			// Add send email button
+			_emailButton = UIButton.FromType(UIButtonType.RoundedRect);
+			_emailButton.Frame = new RectangleF(10, 0, 300, 40);
+			_emailButton.SetTitle("  Send error report by email", UIControlState.Normal);
+			_emailButton.HorizontalAlignment = UIControlContentHorizontalAlignment.Center;
+			_emailButton.TouchDown += new EventHandler(EmailButtonTouchDown);
+			_containerView.AddSubview(_emailButton);
+
+			// Debug details (disabled for release builds)
+			_debugButton = UIButton.FromType(UIButtonType.RoundedRect);
+			_debugButton.Frame = new RectangleF(10, 45, 300, 40);
+			_debugButton.SetTitle(" Debug information", UIControlState.Normal);
+			_debugButton.HorizontalAlignment = UIControlContentHorizontalAlignment.Center;
+			_debugButton.TouchDown += delegate
+			{
+				ModalDialog.Alert("Debug", DebugInfo());
+			};
+
+			_containerView.AddSubview(_debugButton);
+
+			Section section = new Section(_containerView);
+			bindingContext.Root.Add(section);
+		}
+
+		private void EmailButtonTouchDown(object sender, EventArgs e)
+		{
+			if (File.Exists(Settings.Current.LogFile))
+			{
+				if (MFMailComposeViewController.CanSendMail)
+				{
+					NSData data = NSData.FromFile(Settings.Current.LogFile);
+
+					MFMailComposeViewController mailController = new MFMailComposeViewController();
+					mailController.SetSubject("Really simple iPhone error report");
+					mailController.SetToRecipients(new string[] { "mrshrinkray@gmail.com" });
+					mailController.SetMessageBody("Really simple error report", false);
+					mailController.AddAttachmentData(data, "text/plain", "error.log");
+					mailController.Finished += HandleMailFinished;
+					this.PresentModalViewController(mailController, true);
+				}
+				else
+				{
+					ModalDialog.Alert("No email account found", "Please configure an email account before sending an error report");
+				}
+			}
+			else
+			{
+				ModalDialog.Alert("No log file was found", "The error log was empty. Please try sending this again after the error occurs.");
+			}
+		}
+
+		private string DebugInfo()
+		{
+			var allItems = Repository.Default.ListItems();
+			var readItems = allItems.Where(i => !i.IsRead);
+
+			// Image count
+			int imageCount = 0;
+			var dir = new DirectoryInfo(Settings.Current.ImageFolder);
+			var subdirs = dir.GetDirectories();
+			if (subdirs.Length > 0)
+			{
+				imageCount = subdirs[0].GetFiles().Length;
+			}
+
+			StringBuilder builder = new StringBuilder();
+			builder.AppendLine(string.Format("Version {0}", Settings.Current.Version));
+			builder.AppendLine(string.Format("Last updated {0}", Settings.Current.LastUpdate.ToString("HH:mm")));
+			builder.AppendLine(string.Format("Items in the cache {0}", allItems.Count));
+			builder.AppendLine(string.Format("Unread items in the cache {0}", readItems.ToList().Count));
+			builder.AppendLine(string.Format("Images downloaded {0}", imageCount));
+			builder.AppendLine(string.Format("Image downloader working: {0}", ImageDownloader.Current.IsWorking));
+
+			return builder.ToString();
+		}
+
+		void HandleMailFinished(object sender, MFComposeResultEventArgs e)
+		{
+			if (e.Result == MFMailComposeResult.Sent)
+				ModalDialog.Alert("Your email was sent", "");
+
+			e.Controller.DismissModalViewControllerAnimated(true);
 		}
 	}
 }
